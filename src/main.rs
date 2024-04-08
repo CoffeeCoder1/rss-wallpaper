c127919345fbe77ab13df99a95239451318b4e98
Max Nargang <CoffeeCoder1@outlook.com>
2024-04-08 14:11:11 -0400

    Add info logging

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,40 @@ use std::error::Error;
 use rss::Channel;
 use wallpaper;
 use clap::Parser;
+use log::info;
 
 /// Change the current wallpaper to the first image in the latest item in an RSS feed
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 struct Cli {
+    #[command(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
     /// The URL of the RSS feed to parse
     path: String,
 }
 
 async fn feed_parser(feed_url: &str) -> Result<Channel, Box<dyn Error>> {
+    info!("Downloading feed `{:}`", feed_url);
     let content = reqwest::get(feed_url)
         .await?
         .bytes()
         .await?;
+    info!("Parsing feed");
     let channel = Channel::read_from(&content[..])?;
     Ok(channel)
 }
 
 #[tokio::main]
 async fn main() {
+    let cli = Cli::parse();
+    env_logger::Builder::new()
+        .filter_level(cli.verbose.log_level_filter())
+        .init();
+    
     let args = Cli::parse();
     let media_url_binding = feed_parser(&args.path)
             .await
             .expect("Failed to parse RSS feed");
+    info!("Finding media URL");
     let media_url: String = media_url_binding
             .items[0]
             .extensions
@@ -36,7 +47,10 @@ async fn main() {
             .get("url")
             .expect("Media has no URL")
             .to_string();
+    info!("Found media URL `{:}`", media_url);
+    info!("Setting wallpaper");
     tokio::task::spawn_blocking(move || {
         wallpaper::set_from_url(&media_url).unwrap();
     }).await.expect("Failed to set wallpaper from image URL");
+    info!("Wallpaper set");
 }
